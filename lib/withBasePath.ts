f76172cd35c40decde4351fa60c88fa45ba41f20
Oussama Osseili <oussama.osseili@emsi-edu.ma>
2025-10-14 11:34:30 +0100

    Delete lib directory

--- lib/withBasePath.ts
+++
@@ -1,5 +0,0 @@
-export function withBasePath(path: string) {
-  const base = process.env.NEXT_PUBLIC_BASE_PATH || '';
-  const p = path.startsWith('/') ? path : `/${path}`;
-  return `${base}${p}`;
-}
