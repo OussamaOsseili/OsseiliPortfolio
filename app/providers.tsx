ff0ce4f29f8f41908417fbe1e190bd707fceb3b3
Oussama Osseili <oussama.osseili@emsi-edu.ma>
2025-10-14 10:09:17 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,18 @@
 "use client"
 
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { LanguageProvider } from "@/lib/language-context"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <LanguageProvider>{children}</LanguageProvider>
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <LanguageProvider>{children}</LanguageProvider>
+    </NextThemesProvider>
+  )
 }
