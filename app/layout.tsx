49f7cf2f916747e5f732902956b2f4726dae58fe
Oussama Osseili <oussama.osseili@emsi-edu.ma>
2025-10-14 10:04:13 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from "next";
-import "./globals.css"; // ✅ MUST be here so Tailwind styles load
+import type { Metadata } from "next"
+import "./globals.css"
+import Providers from "./providers"
 
 export const metadata: Metadata = {
   title: "Oussama Osseili",
   description: "Portfolio",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  );
+  )
 }
